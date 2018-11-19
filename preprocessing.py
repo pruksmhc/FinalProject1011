5ed7125190ab706fa785e01f64efaeda0cbf18a2
pruksmhc <pruksmhc@mtholyoke.edu>
2018-11-19 15:09:14 -0500

    modified preprocessing so it works

--- preprocessing.py
+++ preprocessing.py
@@ -9,13 +9,32 @@ from operator import itemgetter
 import numpy as np
 import torch
 from torch.utils.data import Dataset
-from sklearn.preprocessing import OneHotEncoder
 from allennlp.commands.elmo import ElmoEmbedder
 import unicodedata
 import re
+import spacy
+import numpy as np
+from underthesea import word_tokenize
+import jieba
+import pdb
+import os
+from elmoformanylangs import Embedder
+import math
+EMBED_DIM = 1024
 
-def load_elmo(all_tokens):
+def preprocess_weights(weights):
+    final_weights = []
+    for i in range(len(weights)):
+        try:
+            if math.isnan(weights[i]):
+                final_weights.append([0]* 1024)
+        except:
+            final_weights.append(np.mean(weights[i], axis=0))
+    return final_weights
+
+def load_elmo(all_tokens, language=""):
     # here, we will load ELMO. 
+    # For Chinese + Vietnamese, we use pretrained https://github.com/HIT-SCIR/ELMoForManyLangs
     all_tokens = list(set(all_tokens))
     current_word2idx = {all_tokens[i]: i+4 for i in range(len(all_tokens))}
     current_word2idx["pad"] = 0
@@ -24,9 +43,23 @@ def load_elmo(all_tokens):
     current_word2idx["<EOS>"] = 3
     weights = []
     elmo = ElmoEmbedder()
-    weights = elmo.embed_sentence(all_tokens)
+    if language == "zh":
+        e = Embedder('179')
+        all_tokens = all_tokens[:10]
+        weights = e.sents2elmo(all_tokens)
+        weights = preprocess_weights(weights)
+        # so this basically returns a 3x 1024 for any words that are 
+        # in the token_embedding
+        # and nan for any words not in the embedding
+    elif language == "vi":
+        pdb.set_trace()
+        e = Embedder('178')
+        weights = e.sents2elmo(all_tokens)
+        weights = preprocess_weights(weights)
+    else: 
+        weights = elmo.embed_sentence(all_tokens)
+        weights = np.mean(weights, axis=0)
     # take the average
-    weights = np.mean(weights, axis=0)
     final_weights = []
     final_weights.append([0]* EMBED_DIM)
     unknown_vector = list(np.random.normal( size=(EMBED_DIM, )))
@@ -36,6 +69,7 @@ def load_elmo(all_tokens):
     final_weights.append(start_vector)
     final_weights.append(end_vector)
     final_weights.extend(weights)
+    pdb.set_trace()
     return current_word2idx, final_weights
 
 SOS_token = 0
@@ -50,8 +84,10 @@ class Lang:
         self.index2word = {0: "sos", 1: "eos"}
         self.n_words = 2  # Count SOS and EOS
 
-    def addSentence(self, sentence):
-        for word in sentence.split(' '):
+    def addSentence(self, sentence, language):
+        # and we have the tokenization here.
+        tokens = token_funcs[language](sentence)
+        for word in tokens:
             self.addWord(word)
 
     def addWord(self, word):
@@ -63,6 +99,21 @@ class Lang:
         else:
             self.word2count[word] += 1
 
+def tokenize_vi(sentence):
+    tokens = word_tokenize(sentence)
+    return tokens
+
+
+def tokenize_zh(sentence):
+    tokens = jieba.cut(sentence, cut_all=True)
+    return tokens
+
+def tokenize_en(sentence):
+    tokenizer = spacy.load('en_core_web_sm')
+    tokens = tokenizer(sentence)
+    return tokens
+
+token_funcs = {"en": tokenize_en, "vi": tokenize_vi, "zh": tokenize_zh}
 # Turn a Unicode string to plain ASCII, thanks to
 # http://stackoverflow.com/a/518232/2809427
 def unicodeToAscii(s):
@@ -101,30 +152,36 @@ def readLangs(lang1, lang2, dataset_type, reverse=False):
         input_lang = Lang(lang1)
         output_lang = Lang(lang2)
     for i in range(len(lang1_file)):
-        input_lang.addSentence(lang1_file[i])
-        output_lang.addSentence(lang2_file[i])
+        # tokenizes based on the language. 
+        input_lang.addSentence(lang1_file[i], lang1)
+        output_lang.addSentence(lang2_file[i], lang2)
 
     pickle.dump(pairs, open("preprocessed_data/iwslt-%s-%s/%s" % (lang1, lang2, dataset_type), "wb"))
     pickle.dump(input_lang, open("preprocessed_data/iwslt-%s-%s/%s_input" % (lang1, lang2, dataset_type), "wb"))
     pickle.dump(output_lang, open("preprocessed_data/iwslt-%s-%s/%s_output" % (lang1, lang2, dataset_type), "wb"))
     return input_lang, output_lang, pairs
-
-
-readLangs("vi", "en", 'dev')
-readLangs("vi", "en", 'train')
-readLangs("vi", "en", 'test')
-
-
-readLangs("zh", "en", 'dev')
-readLangs("zh", "en", 'train')
-readLangs("zh", "en", 'test')
-
-input_pair = pickle.load("preprocessed_data/iwslt-%s-%s/%s_input" % ("vi", "en", "train"))
-output_pair = pickle.load("preprocessed_data/iwslt-%s-%s/%s_output" % ("vi", "en", "train"))
+"""
+output_pair = pickle.load(open("preprocessed_data/iwslt-%s-%s/%s_output" % ("vi", "en", "train"), "rb"))
 # load the ELMO weight embedidngs for ONLY train vocabulary
-all_tokens_en_1 = load_elmo(input_pair.word2count.keys())
-all_tokens_vi = load_elmo(input_pair.word2count.keys())
-pickle.dump(all_tokens_en_1, open("weights_train_en1", "wb"))
-pickle.dump(all_tokens_vi, open("weights_train_vi", "wb"))
+all_tokens_vicurrent_word2idx, all_tokens_vifinal_weights = load_elmo(output_pair.word2count.keys())
+pickle.dump(all_tokens_vifinal_w=eights, open("weights_train_en1", "wb"))
+
+output_pair = pickle.load(open("preprocessed_data/iwslt-%s-%s/%s_output" % ("zh", "en", "train"), "rb"))
+
+pdb.set_trace()
+all_tokens_vicurrent_word2idx, all_tokens_vifinal_weights = load_elmo(list(output_pair.word2count.keys())[5000:])
+pickle.dump(all_tokens_vifinal_weights, open("weights_train_en2_rest", "wb"))
+
+pdb.set_trcae()
+"""
+input_pair = pickle.load(open("preprocessed_data/iwslt-%s-%s/%s_input" % ("zh", "en", "train"), "rb"))
+
+all_tokens_zhcurrent_word2idx, all_tokens_zhfinal_weights = load_elmo(input_pair.word2count.keys(), "zh")
+pickle.dump(all_tokens_zhfinal_weights, open("weights_train_zh", "wb"))
+
+
+input_pair = pickle.load(open("preprocessed_data/iwslt-%s-%s/%s_input" % ("vi", "en", "train"),  "rb"))
+all_tokens_vicurrent_word2idx, all_tokens_vifinal_weights = load_elmo(input_psair.word2count.keys(), "vi")
+pickle.dump(all_tokens_vifinal_weights, open("weights_train_vi", "wb"))
 
 
