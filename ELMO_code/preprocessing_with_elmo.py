b34ee1de7a7bd322d47d9dc9ea37ae1a4eaf1e22
pruksmhc <pruksmhc@mtholyoke.edu>
2018-12-14 21:49:47 -0500

    putting elmo code away

---
+++ ELMO_code/preprocessing_with_elmo.py
@@ -0,0 +1,254 @@
+# now, we tokenize our current dataset
+import pickle
+import pdb 
+import numpy as np
+import pandas as pd
+import pprint
+import numpy as np
+from operator import itemgetter
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from allennlp.commands.elmo import ElmoEmbedder
+import unicodedata
+import re
+import spacy
+import numpy as np
+from underthesea import word_tokenize
+import jieba
+import pdb
+import os
+from elmoformanylangs import Embedder
+import math
+EMBED_DIM = 1024
+
+def preprocess_weights(weights):
+    final_weights = []
+    for i in range(len(weights)):
+        try:
+            if math.isnan(weights[i]):
+                final_weights.append([0]* 1024)
+        except:
+            final_weights.append(np.mean(weights[i], axis=0))
+    return final_weights
+
+def load_elmo(all_tokens, language=""):
+    # here, we will load ELMO. 
+    # For Chinese + Vietnamese, we use pretrained https://github.com/HIT-SCIR/ELMoForManyLangs
+    all_tokens = list(set(all_tokens))
+    current_word2idx = {all_tokens[i]: i+4 for i in range(len(all_tokens))}
+    current_word2idx["unk"] = 1
+    current_word2idx["<SOS>"] = 2
+    current_word2idx["<EOS>"] = 3
+    weights = []
+    elmo = ElmoEmbedder()
+    if language == "zh":
+        e = Embedder('179')
+        weights = e.sents2elmo(all_tokens)
+        weights = preprocess_weights(weights)
+        # so this basically returns a 3x 1024 for any words that are 
+        # in the token_embedding
+        # and nan for any words not in the embedding
+    elif language == "vi":
+        pdb.set_trace()
+        e = Embedder('178')
+        weights = e.sents2elmo(all_tokens)
+        weights = preprocess_weights(weights)
+    else: 
+        weights = elmo.embed_sentence(all_tokens)
+        weights = np.mean(weights, axis=0)
+    # take the average
+    final_weights = []
+    final_weights.append([0]* EMBED_DIM)
+    unknown_vector = list(np.random.normal( size=(EMBED_DIM, )))
+    start_vector = list(np.random.normal(size=(EMBED_DIM, )))
+    end_vector = list(np.random.normal(size=(EMBED_DIM, )))
+    final_weights.append(unknown_vector)
+    final_weights.append(start_vector)
+    final_weights.append(end_vector)
+    final_weights.extend(weights)
+    return current_word2idx, final_weights
+
+SOS_token = 0
+EOS_token = 1
+
+def transform_dict(curr_dict):
+    new_dict = {}
+    new_dict[0] = "pad"
+    new_dict[1] = "unk"
+    for key, value in list(curr_dict.items()):
+        new_dict[key+2] = value
+    new_dict[2] = "<SOS>"
+    new_dict[3] = "<EOS>"
+    return new_dict
+
+class Lang:
+    # This class counts the index to word. 
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "pad", 1: "unk", 2: "<SOS>", 3: "<EOS>"}
+        self.n_words = 2  # so now, let'scount this again. 
+
+    def addSentence(self, sentence, language):
+        # and we have the tokenization here.
+        tokens = token_funcs[language](sentence)
+        for word in tokens:
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+def tokenize_vi(sentence):
+    tokens = word_tokenize(sentence)
+    return tokens
+
+
+def tokenize_zh(sentence):
+    tokens = jieba.cut(sentence, cut_all=True)
+    return tokens
+tokenizer = spacy.load('en_core_web_sm')
+def tokenize_en(sentence):
+    tokens = tokenizer(sentence)
+    final = [t.text for t in tokens]
+    return final
+
+token_funcs = {"en": tokenize_en, "vi": tokenize_vi, "zh": tokenize_zh}
+# Turn a Unicode string to plain ASCII, thanks to
+# http://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+# Lowercase, trim, and remove non-letter characters
+
+def normalizeString(s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+# so normalize the string. 
+def tokenize_indices(lang1, lang2, dataset, index, reverse=False):
+    print("Reading lines...")
+
+    # Read the file and split into lines
+    text_file = pickle.load(open("preprocessed_data/iwslt-%s-%s/%s_%s" % (lang1, lang2, dataset, index), "rb"))
+    output_pair = pickle.load(open("preprocessed_data/iwslt-%s-%s/%s_output" % ("zh", "en", "train"), "rb"))
+    input_pair = pickle.load(open("preprocessed_data/iwslt-%s-%s/%s_input" % ("vi", "en", "train"), "rb"))
+    new_file = []
+    for i in range(len(text_file)):
+        #eos sos, eos sos. 
+        text = text_file[i]
+        word2idx = input_pair.word2index
+        new = []
+        for word in text:
+            if word == "<EOS>":
+                word2idx = output_pair.word2index
+            if word[0] in word2idx.keys():
+                new.append(word2idx[word[0]])
+            else:
+                new.append(word2idx["unk"])
+        new_file.append(new)
+    pdb.set_trace()
+    pickle.dump(new_file, open("preprocessed_data/iwslt-%s-%s/%s_indexed_%s" % (lang1, lang2, dataset, index), "wb"))
+    return new_file
+
+def readLangs(lang1, lang2, dataset_type, reverse=False):
+    print("Reading lines...")
+
+    # Read the file and split into lines
+    lang1_file = open('iwslt-%s-%s/%s.tok.%s' % (lang1, lang2, dataset_type, lang1), encoding='utf-8').\
+        read().strip().split('\n')
+
+    lang2_file = open('iwslt-%s-%s/%s.tok.%s' % (lang1, lang2, dataset_type, lang2), encoding='utf-8').\
+        read().strip().split('\n')
+    # Split every line into pairs and normalize
+    pairs = []
+    dirlink = "preprocessed_data_without_elmo/iwslt-%s-%s/preprocessed_no_indices_pairs_%s" % (lang1, lang2, dataset_type)
+    for i in range(len(lang1_file)):
+        print(i)
+        pair = ["<SOS>"]
+        lang1_text = token_funcs[lang1](lang1_file[i])
+        pair.extend(lang1_text)
+        pair.extend(["<EOS>", "<SOS>"])
+        lang2_text = token_funcs[lang2](normalizeString(lang2_file[i]))
+        pair.extend(lang2_text)
+        pair.extend(["<EOS>"])
+        pairs.append(pair)
+        pickle.dump(pairs, open(dirlink, "wb"))
+    return pairs
+
+"""
+There's something wrong wtiht he actual ELMO embedding:
+With English, 
+output_pair = pickle.load(open("preprocessed_data/iwslt-%s-%s/%s_output" % ("vi", "en", "train"), "rb"))
+# load the ELMO weight embedidngs for ONLY train vocabulary
+all_tokens_vicurrent_word2idx, all_tokens_vifinal_weights = load_elmo(output_pair.word2count.keys())
+pickle.dump(all_tokens_vifinal_w=eights, open("weights_train_en1", "wb"))
+
+output_pair = pickle.load(open("preprocessed_data/iwslt-%s-%s/%s_output" % ("zh", "en", "train"), "rb"))
+
+pdb.set_trace()
+all_tokens_vicurrent_word2idx, all_tokens_vifinal_weights = load_elmo(list(output_pair.word2count.keys())[5000:])
+pickle.dump(all_tokens_vifinal_weights, open("weights_train_en2_rest", "rb"))
+
+pdb.set_trcae()<
+input_lang = pickle.load(open("preprocessed_data/iwslt-%s-%s/train_input" % ("vi", "en"), "rb"))
+output_lang = pickle.load(open("preprocessed_data/iwslt-%s-%s/train_output" % ("vi", "en"), "rb"))
+pdb.set_trace()
+new_train = []
+curr_length = 0
+i = 1
+train = pickle.load(open("preprocessed_data/iwslt-%s-%s/%s_indexed_%s" % ("vi", "en", "train", str(i)), "rb"))
+
+new_train.extend(train)
+pdb.set_trace()
+pickle.dump(new_train, open("preprocessed_data/iwslt-vi-en/train_indexed", "wb"))
+
+lang1 = "vi"
+lang2 = "en"
+dataset = "train"
+indices1 = tokenize_indices(lang1, lang2, dataset,"1")
+indices2 = tokenize_indices(lang1, lang2, dataset,"2")
+indices4 =tokenize_indices(lang1, lang2, dataset,"4")
+indices5 =tokenize_indices(lang1, lang2, dataset,"5")
+indices6 =tokenize_indices(lang1, lang2, dataset,"6")
+indices7 =tokenize_indices(lang1, lang2, dataset,"7")
+indices8 =tokenize_indices(lang1, lang2, dataset,"8")
+indices9 =tokenize_indices(lang1, lang2, dataset,"9")
+indices10 =tokenize_indices(lang1, lang2, dataset,"10")
+indices11 =tokenize_indices(lang1, lang2, dataset,"11")
+"""
+indices1 = pickle.load(open("preprocessed_data/iwslt-vi-en/train_1", "rb"))
+indices2 = pickle.load(open("preprocessed_data/iwslt-vi-en/train_2", "rb"))
+indices4 = pickle.load(open("preprocessed_data/iwslt-vi-en/train_4","rb"))
+indices5 = pickle.load(open("preprocessed_data/iwslt-vi-en/train_5", "rb"))
+indices6 = pickle.load(open("preprocessed_data/iwslt-vi-en/train_6", "rb"))
+indices7 = pickle.load(open("preprocessed_data/iwslt-vi-en/train_7", "rb"))
+indices8 = pickle.load(open("preprocessed_data/iwslt-vi-en/train_8", "rb"))
+indices9 = pickle.load(open("preprocessed_data/iwslt-vi-en/train_9", "rb"))
+indices10 = pickle.load(open("preprocessed_data/iwslt-vi-en/train_10", "rb"))
+indices11 = pickle.load(open("preprocessed_data/iwslt-vi-en/train_11", "rb"))
+indices1.extend(indices2)
+indices1.extend(indices4)
+indices1.extend(indices5)
+indices1.extend(indices6)
+indices1.extend(indices7)
+indices1.extend(indices8)
+indices1.extend(indices9)
+indices1.extend(indices10)
+indices1.extend(indices11)
+pickle.dump(indices1, open("preprocessed_data/iwslt-vi-en/train_tokenized", "wb"))
+
+
+
+
---
+++ ELMO_code/training_with_elmo.py
@@ -0,0 +1,236 @@
+teacher_forcing_ratio = 0.5
+import time
+import math
+import pdb
+import pickle
+from ModelsWithElmo import * 
+import torch
+import torch.nn as nn
+import preprocessing_with_elmo
+from preprocessing_with_elmo import Lang
+import random
+import itertools
+
+MAX_LENGTH_VI_EN = 1310
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+SOS_token = 2
+EOS_token = 3
+
+# ELMO. 
+class Lang:
+    # This class counts the index to word. 
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "pad", 1: "unk", 2: "<SOS>", 3: "<EOS>"}
+        self.n_words = 2  # so now, let'scount this again. 
+
+    def addSentence(self, sentence, language):
+        # and we have the tokenization here.
+        tokens = token_funcs[language](sentence)
+        for word in tokens:
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length,attention=False):
+    encoder_hidden = encoder.initHidden()
+
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    input_length = input_tensor.size(0)
+    target_length = target_tensor.size(0)
+    criterion = nn.NLLLoss()
+
+    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+    # Here, teh outputs is hidden_Size x mmax_lengt
+    # waht is encoder-decoder methodology? 
+    loss = 0
+
+    for ei in range(input_length):
+        # pass in pair by pair rather than using dataloader. 
+        encoder_output, encoder_hidden = encoder(
+            input_tensor[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0, 0]
+    # add SOS before decoding. <eng> EOS SOS <fra>
+    decoder_input = torch.tensor([[SOS_token]], device=device)
+
+    decoder_hidden = encoder_hidden
+    # encoder_hidden is the last hidden state of the encoder. 
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+    if use_teacher_forcing:
+        # Teacher forcing: Feed the target as the next input
+        for di in range(target_length):
+            if attention:
+                decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            else:
+                decoder_output, decoder_hidden, = decoder(
+                decoder_input, decoder_hidden)
+            loss += criterion(decoder_output, target_tensor[di])
+            decoder_input = target_tensor[di]  # Teacher forcing
+    else:
+        # Without teacher forcing: use its own predictions as the next input
+        for di in range(target_length):
+            if attention:
+                decoder_output, decoder_hidden, decoder_attention = decoder(
+                    decoder_input, decoder_hidden, encoder_outputs)
+            else:
+                decoder_output, decoder_hidden, = decoder(decoder_input, decoder_hidden)
+            topv, topi = decoder_output.topk(1) # returns the kelement in the softmax with highest prob
+            decoder_input = topi.squeeze().detach()  # detach from history as input
+            loss += criterion(decoder_output, target_tensor[di])
+            # so only sotp when you see an EOS_token
+            if decoder_input.item() == EOS_token:
+                break
+    loss.backward()
+
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.item() / target_length
+
+
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import matplotlib.ticker as ticker
+import numpy as np
+
+def showPlot(points):
+    plt.figure()
+    fig, ax = plt.subplots()
+    # this locator puts ticks at regular intervals
+    loc = ticker.MultipleLocator(base=0.2)
+    ax.yaxis.set_major_locator(loc)
+    plt.plot(points)
+
+def isplit(iterable,splitters):
+    # gotten from https://stackoverflow.com/questions/4322705/split-a-list-into-nested-lists-on-a-value
+    return [list(g) for k,g in itertools.groupby(iterable,lambda x:x in splitters) if not k]
+
+
+def indexesFromSentence(lang, sentence):
+    sentence_new = []
+    keys = lang.word2index.keys()
+    for word in sentence:
+        if word in keys:
+            sentence_new.append(lang.word2index[word])
+        else:
+            sentence_new.append(1)
+    return sentence_new
+
+
+def tensorFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+
+def trainIters(encoder, decoder, n_iters,n_epochs,  lang1, lang2,  print_every=1000, plot_every=100, learning_rate=0.001):
+    pairs = pickle.load(open("preprocessed_data/iwslt-%s-%s/%s" % (lang1.name, lang2.name, "train_tokenized"), "rb"))
+    dev_pairs = pickle.load(open("preprocessed_data/iwslt-vi-en/dev_tokenized", "rb"))
+    start = time.time()
+    plot_losses = []
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0  # Reset every plot_every
+    encoder_optimizer = torch.optim.SGD(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = torch.optim.SGD(decoder.parameters(), lr=learning_rate)
+    for i in range(n_epochs):
+        # so now, becuase we've already indeed, 
+        training_pairs = [random.choice(pairs) for i in range(n_iters)] # samples the pairs. ?
+        dev_pairs = [random.choice(dev_pairs) for i in range(n_iters)]
+        final_dev_pairs = [isplit(pair,("<EOS>",)) for pair in dev_pairs]
+        pdb.set_trace()
+        # TODO: THIS PAR TWITH VALIDATION IS THE LAST THING THAT DOESN'T QUITE WORK. 
+        input_val = torch.LongTensor([tensorFromSentence(lang1, s[0]) for s in final_dev_pairs])
+        target_val = torch.Tensor([tensorFromSentence(lang1, s[1]) for s in final_dev_pairs])
+        input_val_tensor = torch.FloatTensor()
+        target_val_tensor = torch.from_numpy(np.array(target_val))
+        criterion = nn.NLLLoss()
+        # framing it as a categorical loss function. 
+        for iter in range(1, n_iters + 1):
+            print(iter)
+            training_pair = training_pairs[iter - 1] 
+            training_pair_final = isplit(training_pair,("<EOS>",))
+            # and now we split by the <EOS> tag sicne we hav ethat 
+            d_input_tensor = training_pair_final[0]
+            d_target_tensor = training_pair_final[1]
+            input_tensor = tensorFromSentence(lang1, d_input_tensor)
+            target_tensor = tensorFromSentence(lang2, d_target_tensor)
+            # these eshould be indexed, and wth EOS at the end. 
+
+            # so you have to output the EOS ss well when you're done. 
+            # input is the source language, target is the target language. 
+            loss = train(input_tensor, target_tensor, encoder,
+                         decoder, encoder_optimizer, decoder_optimizer, criterion, MAX_LENGTH_VI_EN)
+            print_loss_total += loss
+            plot_loss_total += loss
+
+            if iter % print_every == 0:
+                print_loss_avg = print_loss_total / print_every
+                print_loss_total = 0
+                print('TRAIN SCORE %s (%d %d%%) %.4f' % (timeSince(start, iter / n_epochs),
+                                             iter, iter / n_epochs * 100, print_loss_avg))
+                val_loss = train(input_val_tensor, target_val_tensor, encoder,
+                decoder, encoder_optimizer, decoder_optimizer, criterion, MAX_LENGTH_VI_EN)
+                print("VALIDATION SCORE: "+str(val_loss))
+
+            if iter % plot_every == 0:
+                plot_loss_avg = plot_loss_total / plot_every
+                plot_losses.append(plot_loss_avg)
+                plot_loss_total = 0
+
+    showPlot(plot_losses)
+# get the tokenzied parts. 
+# Finishign the inference function. 
+def trainNoAttention(lang1, lang2):
+    hidden_size = 1024
+    train_data = pickle.load(open("preprocessed_data/iwslt-vi-en/train_indexed", "rb"))
+    lang_object_input =  pickle.load(open("preprocessed_data/iwslt-"+lang1+"-"+lang2+"/train_input", "rb"))
+    lang_object_output =  pickle.load(open("preprocessed_data/iwslt-"+lang1+"-"+lang2+"/train_output", "rb"))
+    # attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
+
+    weights_l1_torch = torch.load("weights_"+lang1+"_torch")
+    weights_l2_torch = torch.load("weights_"+lang2+"_torch")
+    encoder = EncoderRNN(lang_object_input.n_words, hidden_size, weights_l1_torch).to(device)
+    decoder = DecoderRNN(hidden_size, lang_object_output.n_words, weights_l2_torch).to(device)
+    trainIters(encoder, decoder, 750, 10, lang_object_input, lang_object_output, print_every=5) #train on a small subset
+
+def trainWithAttention(lang1, lang2):
+    hidden_size = 1024
+    train_data = pickle.load(open("preprocessed_data/iwslt-vi-en/train_indexed", "rb"))
+    lang_object_input =  pickle.load(open("preprocessed_data/iwslt-"+lang1+"-"+lang2+"/train_input", "rb"))
+    lang_object_output =  pickle.load(open("preprocessed_data/iwslt-"+lang1+"-"+lang2+"/train_output", "rb"))
+    # attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
+
+    weights_en_torch = torch.load("weights_en_torch")
+    weights_vi_torch = torch.load("weights_vi_torch")
+    encoder = EncoderRNN(lang_object_input.n_words, hidden_size, weights_vi_torch).to(device)
+    decoder = DecoderRNN(hidden_size, lang_object_output.n_words, weights_en_torch).to(device)
+    trainIters(encoder, decoder, 750, 10, lang_object_input, lang_object_output, print_every=5) #train on a small subset
+
+
+trainNoAttention("vi", "en")
+
